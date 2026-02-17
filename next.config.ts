524bc57011cce462cf60ffe5484130ddd8b122b4
jasonkdeng <jasonkaideng@gmail.com>
2026-02-17 15:24:19 -0500

    routing fix for home

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,25 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  async redirects() {
+    return [
+      {
+        source: "/index.html",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/",
+          destination: "/index.html",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
